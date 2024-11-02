9fd27eeddf31777aea66d9fdbe4347ec5f23fd8f
phatsanphonna <phatsanphon2004@outlook.com>
2024-11-02 17:59:18 +0700

    feat: provide crendentials

--- src/lib/server/s3/index.ts
+++ src/lib/server/s3/index.ts
@@ -1,5 +1,11 @@
+import { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY } from '$env/static/private';
 import { S3Client } from '@aws-sdk/client-s3';
 
 export const s3 = new S3Client({
-  region: 'us-east-1',
-})
+	region: 'us-east-1',
+	credentials: {
+		accessKeyId: AWS_ACCESS_KEY_ID,
+		secretAccessKey: AWS_SECRET_ACCESS_KEY
+	}
+});
+
